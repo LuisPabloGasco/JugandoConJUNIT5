5ee29aac20a9e31ba7231f726f3e9fb4f62f63a7
LuisPabloGasco <lpg1983@gmail.com>
2023-10-14 10:41:32 -0300

    sdf

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,16 @@
+
+public class Calculadora {
+
+	public Calculadora() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public int sumarDosNumeros(int numero1, int numero2) {
+		return numero1 + numero2;
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/Calculadora_TEST.java
@@ -0,0 +1,49 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Calculadora_TEST {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+
+	@Test
+	@DisplayName("Test UNO")
+	void testeo() {
+		fail("Not yet implemented");
+		//jadfhdasg
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
